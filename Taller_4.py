2b876be8542bc9b93fc741040a206e8bf41173f9
EmanuelFonseca2023 <emanueldavidfonsecamontagut@gmail.com>
2025-03-20 20:41:15 -0500

    Creacion de las funciones CRUD

--- Taller_4.py
+++ Taller_4.py
@@ -4,3 +4,22 @@ con = sqlite3.connect("DataBase.db")
 cur = con.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,telefono INTEGER,email TEXT,ciudad TEXT,direccion TEXT)""")
 con.commit()
+#Se crean las funciones CRUD
+def create_users(name,telefono,email,ciudad,direccion):
+    cur.execute("INSERT INTO users(name,telefono,email,ciudad,direccion) VALUES (?,?,?,?,?)",(name,telefono,email,ciudad,direccion))
+    con.commit()
+def leer_users():
+    cur.execute("SELECT * FROM users")
+    return cur.fetchall()
+def actualizar_users(id,telefono,email,ciudad,direccion):
+    cur.execute("UPDATE users SET telefono=? ,email=?,ciudad=?,direccion=? WHERE id =?",(telefono,email,ciudad,direccion,id))
+    con.commit()
+def actualizar_telefono(id,telefono):
+    cur.execute("UPDATE users SET telefono=? WHERE id =?",(telefono,id))
+    con.commit()
+def actualizar_email(id,email):
+    cur.execute("UPDATE users SET email=? WHERE id =?",(email,id))
+    con.commit()
+def borrar_users(id):
+    cur.execute("DELETE FROM users WHERE id =?",(id,))
+    con.commit()
